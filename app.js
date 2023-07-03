ecb2e9ba3817694a88970e3f6b97b7adb2e3c4a9
daniel-su1 <107098486+daniel-su1@users.noreply.github.com>
2023-07-03 03:30:39 -0400

    Implemented authentication and login functionality

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
 const Spot = require('./models/spot');
 const Review = require('./models/review');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-const spots = require('./routes/spots');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const spotRoutes = require('./routes/spots');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://127.0.0.1:27017/study-spotter', {
     useNewUrlParser: true,
@@ -48,14 +52,22 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser()); // how to store user in session
+passport.deserializeUser(User.deserializeUser()); // how to get user out of session
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error');
     next();
 })
 
-app.use('/spots', spots);
-app.use('/spots/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/spots', spotRoutes);
+app.use('/spots/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
--- routes/spots.js
+++ routes/spots.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/CatchAsync')
 const {spotSchema} = require('../schemas.js')
+const isLoggedIn = require('../middleware.js');
 const ExpressError = require('../utils/ExpressError')
 const Spot = require('../models/spot');
 
@@ -22,11 +23,11 @@ router.get('/', async (req, res) => {
     res.render('spots/index', { spots })
 })
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('spots/new');
 });
 
-router.post('/', validateSpot, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateSpot, catchAsync(async (req, res, next) => {
     const spot = new Spot(req.body.spot);
     await spot.save();
     req.flash('success', 'Successfully made a new spot!');
@@ -43,19 +44,19 @@ router.get('/:id', catchAsync(async (req, res) => {
     res.render('spots/show', { spot });
 }))
 
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const spot = await Spot.findById(req.params.id);
     res.render('spots/edit', { spot });
 }))
 
-router.put('/:id', validateSpot, catchAsync(async (req, res) => {
+router.put('/:id', validateSpot, isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     const spot = await Spot.findByIdAndUpdate(id, { ...req.body.spot })
     req.flash('success', 'Successfully updated spot!');
     res.redirect(`/spots/${spot._id}`)
 }))
 
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Spot.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted spot!')
